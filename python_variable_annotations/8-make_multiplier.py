826f42fa5a2ee033710bb82b572a310a2ba5ea68
Samuel Chen <sche842@gmail.com>
2026-08-02 03:05:33 +1000

    fix type err

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,7 +4,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[[float], [float]]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns a function that multiplies a float by a multiplier"""
 
     def multiply(value: float) -> float:
