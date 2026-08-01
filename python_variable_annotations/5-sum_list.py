042f31ece1d037d701f4d6b96c559cb6b4d1307a
Samuel Chen <sche842@gmail.com>
2026-08-02 02:50:05 +1000

    sum list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module defines function that returns sum of a list"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of all floats in list"""
+    return sum(input_list)
