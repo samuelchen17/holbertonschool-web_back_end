d8db563694b037f9d0820a1be11af6b23303686d
Samuel Chen <sche842@gmail.com>
2026-08-02 02:42:55 +1000

    float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module contains function that returns floor of a float"""
+
+import math
+
+
+def concat(n: float) -> int:
+    """return floor of a float"""
+    return math.floor(n)
