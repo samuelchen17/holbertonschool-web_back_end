3dad6e174b1c014fd7eb444e35895d54422e3117
Samuel Chen <sche842@gmail.com>
2026-08-02 02:45:32 +1000

    float to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module contains function that converts float to string"""
+
+
+def to_str(n: float) -> str:
+    """return str representation of a float"""
+    return str(n)
