cdb126fa95342dd3c46a3f7987332f95b64a04fe
Samuel Chen <sche842@gmail.com>
2026-08-02 02:51:40 +1000

    sum mixed list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module defines function that returns sum of a list"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """return sum of all floats in list"""
+    return float(sum(mxd_lst))
