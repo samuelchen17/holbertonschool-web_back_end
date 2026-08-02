fbd2c4bb05a1c34a69b530603973df8733618ac8
Samuel Chen <sche842@gmail.com>
2026-08-02 17:28:29 +1000

    async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module defines an async generator"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None, None]:
+    """this function asynchronously generates random numbers"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
