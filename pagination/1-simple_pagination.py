040cc8848f2e749752cbceb2eb4b00b99e74a3c9
Samuel Chen <sche842@gmail.com>
2026-08-14 01:59:35 +1000

    change assert order

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -33,8 +33,8 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """return list of rows belonging to requested page"""
-        assert page > 0 and isinstance(page, int)
-        assert page_size > 0 and isinstance(page_size, int)
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
 
