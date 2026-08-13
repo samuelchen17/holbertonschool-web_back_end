5de3966532cf34de62525113b08a9aed0acc58b4
Samuel Chen <sche842@gmail.com>
2026-08-14 01:48:17 +1000

    test

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -37,13 +37,14 @@ class Server:
 
 def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
     """return list of rows belonging to requested page"""
-    assert page > 0 and isinstance(
-        page, int
-    ), "Page must be int and greater than 0"
-    assert page_size > 0 and isinstance(
-        page_size, int
-    ), "Page size myst be in and greater than 0"
+    assert page > 0 and isinstance(page, int)
+    assert page_size > 0 and isinstance(page_size, int)
 
     start, end = index_range(page, page_size)
 
-    return self.dataset()[start:end]
+    dataset = self.dataset()
+
+    if start >= len(dataset):
+        return []
+
+    return dataset[start:end]
