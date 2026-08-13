5355c8d116d9c2ea809b37d514886563202af50c
Samuel Chen <sche842@gmail.com>
2026-08-14 01:45:52 +1000

    test

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -46,7 +46,4 @@ def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
 
     start, end = index_range(page, page_size)
 
-    if start >= len(self.dataset()):
-        return []
-
     return self.dataset()[start:end]
