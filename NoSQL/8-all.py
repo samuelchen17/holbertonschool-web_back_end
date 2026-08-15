a77e725ca47cfe1b596d66bd2cf85bc75d7328aa
Samuel Chen <sche842@gmail.com>
2026-08-15 14:02:02 +1000

    8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for listing all docs in MongoDB collection"""
+
+
+def list_all(mongo_collection):
+    """list all docs in collection"""
+    return list(mongo_collection.find())
