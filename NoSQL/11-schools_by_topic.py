a9b22537757b7c09d989a6fe0551eed6160c0101
Samuel Chen <sche842@gmail.com>
2026-08-15 14:09:52 +1000

    find

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that lists all schools with a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """return list of schools having a specific topic"""
+
+    mongo_collection.find({"topics": topic})
