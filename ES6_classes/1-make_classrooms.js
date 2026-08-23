48dec948c7f80641bf43b4e73feb82531978869f
Samuel Chen <sche842@gmail.com>
2026-08-23 13:21:59 +1000

    1

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from "./0-classroom.js"
+
+const initializeRooms = () => {
+    let classRooms = [
+        new ClassRoom(19), 
+        new ClassRoom(20), 
+        new ClassRoom(34)
+    ]
+
+    return classRooms;
+}
