c70ee732092c22c18ea65f32580a892a32023cbe
Samuel Chen <sche842@gmail.com>
2026-08-29 01:57:31 +1000

    2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+const handleResponseFromAPI = (promise) => {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+};
+
+export default handleResponseFromAPI;
