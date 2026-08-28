e69d66882eb1c4686013350db32bc58ac416ece7
Samuel Chen <sche842@gmail.com>
2026-08-29 02:13:35 +1000

    test

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,9 @@
-import uploadPhoto from './utils.js';
-import createUser from './utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
 const handleProfileSignup = () => {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
-      console.log(photo.body, user.firstName, user.lastName);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
