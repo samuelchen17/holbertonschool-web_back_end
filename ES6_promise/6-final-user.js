821676bcdddf40008f8b0eca6566de072a21e952
Samuel Chen <sche842@gmail.com>
2026-08-29 02:32:55 +1000

    6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+const handleProfileSignup = (firstName, lastName, fileName) => {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value:
+        result.status === 'fulfilled' ? result.value : result.reason.toString(),
+    })),
+  );
+};
+
+export default handleProfileSignup;
