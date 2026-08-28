1452fab61539ce8a667743c305fdcde8ef646b11
Samuel Chen <sche842@gmail.com>
2026-08-28 23:45:48 +1000

    update

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,20 @@
+const updateStudentGradeByCity = (students, city, newGrades) => {
+  // filter to get students from the city
+  // go through each one using map
+  // find the matching id from newGrades
+  // conditional statement to see if new grade exists, if yes update the grade, else N/A
+  // return the filtered students with new grade updated
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = newGrades.find(
+        (grade) => grade.studentId === student.id,
+      );
+
+      const updatedGrade = studentGrade ? studentGrade.grade : ' N/A';
+
+      return { ...student, grade: updatedGrade };
+    });
+};
+
+export default updateStudentGradeByCity;
