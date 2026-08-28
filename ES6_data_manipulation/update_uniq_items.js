f0982f380b8c257aff4d45ab5ac30651a8a7f1ff
Samuel Chen <sche842@gmail.com>
2026-08-29 01:01:10 +1000

    update quant

---
+++ ES6_data_manipulation/update_uniq_items.js
@@ -0,0 +1,15 @@
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  map.forEach((quantity, item) => {
+    if (quantity === 1) {
+      map.set(item, 100);
+    }
+  });
+
+  return map;
+};
+
+export default updateUniqueItems;
