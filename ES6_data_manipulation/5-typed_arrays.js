11cd66ffa33dfa429987f3c3cd009b0cc1d10f57
Samuel Chen <sche842@gmail.com>
2026-08-28 23:58:17 +1000

    fix condition

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,10 @@
 const createInt8TypedArray = (length, position, value) => {
+  // create buffer
   const buffer = new ArrayBuffer(length);
+  // Create an Int8Array view of the buffer
   const view = new Int8Array(buffer);
 
-  if (position >= length) {
+  if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
   view[position] = value;
