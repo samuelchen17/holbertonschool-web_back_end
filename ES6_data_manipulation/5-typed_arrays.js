2688270cfa03ea173f95ffc3abf6ccf5b28b46a8
Samuel Chen <sche842@gmail.com>
2026-08-28 23:56:46 +1000

    5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+const createInt8TypedArray = (length, position, value) => {
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  view[position] = value;
+
+  return buffer;
+};
+
+export default createInt8TypedArray;
