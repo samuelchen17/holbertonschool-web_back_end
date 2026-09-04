e8f90a9b239f354b49195e2c417e93194fdfddbd
Samuel Chen <sche842@gmail.com>
2026-09-05 00:41:41 +1000

    test

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const students = lines.slice(1);
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+
+    students.forEach((student) => {
+      const [firstName, , , field] = student.split(',');
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(firstName);
+    });
+
+    Object.keys(fields).forEach((field) => {
+      console.log(
+        `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
+      );
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
