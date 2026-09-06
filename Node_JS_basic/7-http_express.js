3ea652cb75bb69c69ebc384dae8007ab2177825b
Samuel Chen <sche842@gmail.com>
2026-09-06 18:54:37 +1000

    7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,9 +2,23 @@ const express = require('express');
 
 const app = express();
 const PORT = 1245;
+const DB_FILE = process.argv[2];
+const countStudents = require('./3-read_file_async');
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
+});
+
+app.get('/students',async (req, res) => {
+  const body = ['This is the list of our students'];
+
+  try {
+    const data = await countStudents(DB_FILE);
+    body.push(data);
+    res.end(body.join('\n'));
+  } catch (err) {
+    res.end(`${body.join('\n')}\n${err.message}`);
+  }
 });
 
 app.listen(PORT);
