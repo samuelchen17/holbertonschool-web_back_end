ba12ece44cef5971be2db8eff17624d7417eaee3
Samuel Chen <sche842@gmail.com>
2026-09-06 23:27:14 +1000

    revert

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,7 @@
-export default class AppController {
-  static getHomepage(req, res) {
-    res.status(200).send('Hello ALX!');
+class AppController {
+  static getHomepage(request, response) {
+    return response.status(200).send('Hello Holberton School!');
   }
 }
+
+export default AppController;
