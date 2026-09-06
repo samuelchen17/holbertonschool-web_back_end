d2a259530074b653f9efb7ca77c219fec485bf37
Samuel Chen <sche842@gmail.com>
2026-09-06 18:57:51 +1000

    fix

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,13 +3,53 @@ const express = require('express');
 const app = express();
 const PORT = 1245;
 const DB_FILE = process.argv[2];
-const countStudents = require('./3-read_file_async');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (error, fileContent) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = fileContent
+        .split('\n')
+        .filter((line) => line.trim() !== '');
+
+      const students = lines.slice(1);
+      const total = students.length;
+
+      const fields = {};
+
+      for (const student of students) {
+        const studentData = student.split(',');
+        const firstName = studentData[0];
+        const field = studentData[3];
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstName);
+      }
+
+      const studentData = [];
+      studentData.push(`Number of students: ${total}`);
+      Object.keys(fields).forEach((field) => {
+        studentData.push(
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
+        );
+      });
+      resolve(studentData.join('\n'));
+    });
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students',async (req, res) => {
+app.get('/students', async (req, res) => {
   const body = ['This is the list of our students'];
 
   try {
